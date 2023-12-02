39da06cb0cf004d588ac314e657445048ec5eab8
Ionuț Staicu <github@iamntz.com>
2023-12-02 18:36:49 +0200

    initial commit

---
+++ config.h
@@ -0,0 +1,44 @@
+// Copyright 2023 Ionuț Staicu (@iamntz)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/* Split Defines */
+#define MASTER_LEFT
+// #define EE_HANDS
+
+#define USE_SERIAL
+
+#define SERIAL_USART_FULL_DUPLEX
+
+#define SERIAL_USART_TX_PIN GP1
+#define SERIAL_USART_RX_PIN GP0
+#define SERIAL_USART_PIN_SWAP
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+// #define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+// #define LOCKING_RESYNC_ENABLE
+// #define SPLIT_HAND_PIN GP4
+// #define USB_VBUS_PIN GP8
+
+// #define I2C_DRIVER I2CD2
+// #define I2C1_SDA_PIN GP10
+// #define I2C1_SCL_PIN GP11
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Compile:
+
+```
+qmk compile -kb ntz_split_1 -km default 
+```
+
+
+## Flash
+
+Press ESC while plugging in & a new drive will show up. 
+
+Copy the `.build/*.uf2` file to the RPI.
+
+
+### Stuff
+
+- https://github.com/qmk/qmk_firmware/blob/master/docs/keycodes_basic.md
